ab0ed821913a38386ba244ad3de1ce14d7041b88
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-12-16 13:34:13 +0530

    config order-service

---
+++ services/order/src/main/java/com/eranda/ecommerce/order/PaymentMethod.java
@@ -0,0 +1,10 @@
+package com.eranda.ecommerce.order;
+
+public enum PaymentMethod {
+
+    PAYPAL,
+    CREDIT_CARD,
+    VISA_CARD,
+    MASTERCARD,
+    BITCOIN
+}
---
+++ services/order/src/main/java/com/eranda/ecommerce/orderline/OrderLine.java
@@ -0,0 +1,25 @@
+package com.eranda.ecommerce.orderline;
+
+import com.eranda.ecommerce.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer productId;
+    private double quantity;
+}
