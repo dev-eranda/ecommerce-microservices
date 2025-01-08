1476265da3ec7269b061ce30e3d507e3a75753d3
ErandaMadusanka <emsamarasinghe@gmail.com>
2025-01-08 13:12:45 +0530

    add keycloak to auth the gateway

--- services/notification/src/main/java/com/eranda/ecommerce/email/EmailService.java
+++ services/notification/src/main/java/com/eranda/ecommerce/email/EmailService.java
@@ -5,6 +5,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
